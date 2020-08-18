71e488e1a4126378a285fc9b3b15a98b6a7d0832
Mathieu Valery <mathieuromainvalery@gmail.com>
2020-08-18 17:47:04 +0200

    set accepted bookings to default false

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
+  validates :description, presence: true
   belongs_to :user
   belongs_to :trip
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_150510) do
+ActiveRecord::Schema.define(version: 2020_08_18_154317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_150510) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "accepted"
+    t.boolean "accepted", default: false
     t.bigint "user_id", null: false
     t.bigint "trip_id", null: false
     t.datetime "created_at", precision: 6, null: false
