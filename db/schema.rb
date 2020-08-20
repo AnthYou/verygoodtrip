86e7f9baf27375510efe90cba66d6880fe4bb501
gourouhassiba <gourouhassiba@gmail.com>
2020-08-20 15:36:39 +0200

    add code to add a map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_154317) do
+ActiveRecord::Schema.define(version: 2020_08_20_094821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_154317) do
     t.date "departure_date"
     t.date "return_date"
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
