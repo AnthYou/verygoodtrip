12edccccd67cea2b588cdade1464ddb027b9cc5d
Mathieu Valery <mathieuromainvalery@gmail.com>
2020-08-20 12:51:20 +0200

    add validation for unique booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   validates :description, presence: true
+  validates :user_id, uniqueness: { scope: :trip_id,
+    message: "already joined this trip" }
   belongs_to :user
   belongs_to :trip
 end
