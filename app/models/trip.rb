2cd0cd37c7c3a3ac1d28b4fc1b05c2939a8f5a6d
Anthony You <you.anthony@yahoo.com>
2020-08-20 17:15:40 +0200

    added validation on trip photo

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,17 +3,19 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, presence: true
-  validates :description, presence: true, length: { minimum: 100 }
+  validates :description, presence: true, length: { minimum: 30 }
   validates :destination, presence: true
   validates :budget_min, presence: true
   validates :budget_max, presence: true
   validates :departure_date, presence: true
   validates :return_date, presence: true
-  validates :capacity, presence: true
+  validates :capacity, presence: true, numericality: { greater_than: 1 }
+  validates :photo, presence: true
   validate :end_date_after_start_date # error message is not displayed yet in simple form
 
   geocoded_by :destination
   after_validation :geocode, if: :will_save_change_to_destination?
+
   private
 
   def end_date_after_start_date
