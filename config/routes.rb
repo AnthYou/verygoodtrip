1fa03da7590d1b9fbcab4f4789d15c5835c8beed
Raphael Toubiana <raphael.toubiana@gmail.com>
2020-08-20 14:35:37 +0200

    update routes and add registrations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   root to: 'pages#home'
   resources :trips, only: [ :index, :create, :new, :show] do
     resources :bookings, only: [:new, :create]
